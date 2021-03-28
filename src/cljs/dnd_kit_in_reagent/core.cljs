b3acb5adf0f0ee2e700b3b8b494b1bdf74c01bea
Aaron Decker <decker49@gmail.com>
2021-03-27 20:42:44 -0500

    Add droppable and draggable.

--- src/cljs/dnd_kit_in_reagent/core.cljs
+++ src/cljs/dnd_kit_in_reagent/core.cljs
@@ -1,10 +1,26 @@
 (ns dnd-kit-in-reagent.core
   (:require
-   [reagent.core :as reagent :refer [atom]]
-   [reagent.dom :as rdom]))
+   [reagent.core :as r]
+   [reagent.dom :as rdom]
+   ["@dnd-kit/core" :as dnd-kit :refer [DndContext useDraggable useDroppable]]
+   ["@dnd-kit/utilities" :refer [CSS]]))
+
+(defn droppable []
+  (let [{:keys [isOver setNodeRef]} (js->clj (useDroppable (clj->js {:id "droppable"})))]
+    (r/as-element
+     [:div {} "Droppable element"])))
+
+(defn draggable []
+  (let [{:keys [attributes listeners setNodeRef transform]} (js->clj (useDraggable (clj->js {:id "draggable"})))
+        style {:transform (.toString (.-Translate CSS) transform)}]
+    (r/as-element
+     [:div {:style style} (str "Draggable element: " style)])))
 
 (defn app []
-  [:div {} "app goes here"])
+  [:> DndContext
+   [:> droppable]
+   [:> draggable]
+   ])
 
 (defn mount-root []
   (rdom/render [app] (.getElementById js/document "app")))
